7ad505bfb14ae41499fc2973769091ca3917fbcb
Nishant Choudhary <n1sh4ntch0udh4ry@gmail.com>
2020-06-02 02:14:32 +0530

    add collection preview and item component

---
+++ src/components/preview-collection/collection-preview.component.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import CollectionItem from '../collection-item/collection-item.component'
+
+import './collection-preview.styles.scss'
+
+const CollectionPreview = ({id, title, items}) => (
+    <div className="collection-preview">
+        <h1>{title.toUpperCase()}</h1>
+        <div className="preview">
+        {
+            items
+            .filter((items,idx) => idx<4)
+            .map(
+            ({id, ...otherItemProps}) => (
+                <CollectionItem key={id}{ ...otherItemProps }></CollectionItem>
+            )
+            )
+        }
+        </div>
+    </div>
+)
+
+export default CollectionPreview;
+
+
+
