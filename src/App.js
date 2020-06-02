7174fb36346060d37e837d6827c8282602ab9dae
Nishant Choudhary <n1sh4ntch0udh4ry@gmail.com>
2020-06-03 04:41:56 +0530

    implement firebase and add sign up

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import ShopPage from './pages/shop/shop.component';
 import { Switch, Route } from 'react-router-dom';
 import SignInAndSignUp from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 import Header from './components/header/header.component'
-import {auth} from './firebase/firebase.utls';
+import {auth, createUserProfileDocument} from './firebase/firebase.utls';
 
 class App extends React.Component{
 
@@ -20,11 +20,26 @@ class App extends React.Component{
 
 
   componentDidMount() {
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
-      this.setState({currentUser: user})
-      console.log(user);
-    })
-  }
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+     if (userAuth){
+       
+       const userRef = await createUserProfileDocument(userAuth);
+       userRef.onSnapshot(snapShot => {
+         this.setState({
+           currentUser: {
+             id: snapShot.id,
+            ...snapShot.data()
+           }
+         })
+       })
+     }
+     else {
+      this.setState({currentUser: userAuth});
+    }
+     
+    } 
+    
+    )}
 
   componentWillUnmount() {
     this.unsubscribeFromAuth();
