447fa15bdd4dc7b675d7f9766a82bec9a622ab37
Nishant Choudhary <n1sh4ntch0udh4ry@gmail.com>
2020-06-04 03:06:01 +0530

    add redirect for signin

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
-import { Switch, Route } from 'react-router-dom';
+import { Switch, Route, Redirect } from 'react-router-dom';
 import SignInAndSignUp from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 import Header from './components/header/header.component'
 import {auth, createUserProfileDocument} from './firebase/firebase.utls';
@@ -43,14 +43,18 @@ class App extends React.Component{
    <Switch>
      <Route exact path="/" component={HomePage}/>
      <Route path="/shop" component={ShopPage}/>
-     <Route path="/signin" component={SignInAndSignUp}/>
+     <Route exact path="/signin" render={()=> this.props.currentUser ? (<Redirect to='/'/>) : (<SignInAndSignUp/>) } />
    </Switch>
    </div>
   );
   }}
 
+const mapStateToProps = ({user}) => ({
+  currentUser: user.currentUser
+})
+
 const mapDispatchToProps = dispatch => ({
     setCurrentUser: user => dispatch(setCurrentUser(user))
 })
 
-export default connect(null, mapDispatchToProps)(App);
+export default connect(mapStateToProps, mapDispatchToProps)(App);
