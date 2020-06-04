b96a33467982125077d5f3b719cf7aa1caba6fe4
Nishant Choudhary <n1sh4ntch0udh4ry@gmail.com>
2020-06-05 00:42:40 +0530

    create items component and utls - connect dropdown to cart reducer

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,16 +1,24 @@
 import React from 'react';
 import './cart-dropdown.styles.scss';
-
+import CartItem from '../cart-item/cart-item.components';
+import { connect } from 'react-redux';
 import CustomButton from '../custom-button/custom-button.component';
 
-const CartDropdown = () => (
+const CartDropdown = ({cartItems}) => (
     <div className="cart-dropdown"> 
     <div className='cart-items'>
-
+            {
+                cartItems.map(cartItem => <CartItem key={CartItem.id} item={cartItem} />)
+                
+                }
     </div>
 
     <CustomButton>GO TO CHECKOUT</CustomButton>
     </div>
 )
 
-export default CartDropdown;
+const mapStateToProps = ({cart: {cartItems}}) => ({
+    cartItems : cartItems
+})
+
+export default connect(mapStateToProps)(CartDropdown);
