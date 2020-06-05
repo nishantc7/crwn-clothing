e25534a57cd840e2131cc4ffa623c5ec42c15c49
Nishant Choudhary <n1sh4ntch0udh4ry@gmail.com>
2020-06-05 23:45:16 +0530

    set collection paths and normailized data

--- src/redux/shop/shop.selector.js
+++ src/redux/shop/shop.selector.js
@@ -1,12 +1,5 @@
 import {createSelector} from 'reselect'
 
-const COLLECTTION_ID_MAP = {
-    hats:1,
-    sneakers:2,
-    jackets:3,
-    womens:4,
-    mens:5
-}
 
 const selectShop = state => state.shop
 
@@ -19,7 +12,6 @@ export const selectCollections = createSelector(
 export const selectCollection = collectionsUrlParam => 
 createSelector(
     [selectCollections],
-    collections => collections.find(
-       collection => collection.id === COLLECTTION_ID_MAP[collectionsUrlParam]
-    )
+    collections => collections[collectionsUrlParam]
+    
 )
