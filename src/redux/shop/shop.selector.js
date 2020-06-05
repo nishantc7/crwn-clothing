f36e6c9868c8d9cca1814648f56ecac501fba1f1
Nishant Choudhary <n1sh4ntch0udh4ry@gmail.com>
2020-06-05 20:52:46 +0530

    refactor and separate collection

--- src/redux/shop/shop.selector.js
+++ src/redux/shop/shop.selector.js
@@ -1,8 +1,25 @@
 import {createSelector} from 'reselect'
 
+const COLLECTTION_ID_MAP = {
+    hats:1,
+    sneakers:2,
+    jackets:3,
+    womens:4,
+    mens:5
+}
+
 const selectShop = state => state.shop
 
 export const selectCollections = createSelector(
     [selectShop],
     shop => shop.collections
+)
+
+
+export const selectCollection = collectionsUrlParam => 
+createSelector(
+    [selectCollections],
+    collections => collections.find(
+       collection => collection.id === COLLECTTION_ID_MAP[collectionsUrlParam]
+    )
 )
